22b0e8aa90284cca2069ec6141010c61210524ea
Bret Fernberg <bret.fernberg@gmail.com>
2021-04-21 13:22:55 -0600

    Finished w/Ben in lecture

--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -3,10 +3,17 @@ import React, { Component } from 'react';
 export default class ColorChanger extends Component {
   render() {
     return (
-      <select className="dropDownContainer">
+      <select 
+        onChange={(e)=>this.props.update(+e.target.value)} 
+        disabled={this.props.allowEdit === 'false'? true: false} 
+        className="dropDownContainer"
+        >
         <option value="12"> 12 </option>
         <option value="13"> 13 </option>
         <option value="14"> 14 </option>
+        <option value="15"> 15 </option>
+        <option value="16"> 16 </option>
+        <option value="17"> 17 </option>
       </select>
     );
   }
